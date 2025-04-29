e1c0bf03832530623da744f1a7096a9e685c6a0f
Hedi-Slm <hedi.slm42@gmail.com>
2025-04-29 21:49:09 +0200

    Fixed logic issues for past competition handling

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 from datetime import datetime
 
 
-def is_past_competition(competition_date):
+def is_past_competition_func(competition_date):
     comp_date = datetime.strptime(competition_date, "%Y-%m-%d %H:%M:%S")
     current_date = datetime.now()
     return comp_date < current_date
@@ -39,7 +39,8 @@ def show_summary():
     if not club:
         flash("Sorry, that email wasn't found.")
         return render_template('index.html')
-    return render_template('welcome.html', club=club[0], competitions=competitions)
+    return render_template('welcome.html', club=club[0], competitions=competitions,
+                           is_past_competition=is_past_competition_func)
 
 
 
@@ -48,7 +49,7 @@ def book(competition, club):
     found_club = [c for c in clubs if c['name'] == club][0]
     found_competition = [c for c in competitions if c['name'] == competition][0]
     if found_club and found_competition:
-        if is_past_competition(found_competition['date']):
+        if is_past_competition_func(found_competition['date']):
             flash("Cannot book places for past competitions")
             return render_template('welcome.html', club=club, competitions=competitions)
         else:
@@ -64,30 +65,32 @@ def purchase_places():
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     club_points = int(club['points'])
 
-    if is_past_competition(competition['date']):
+    if is_past_competition_func(competition['date']):
         flash("Cannot book places for past competitions")
         return render_template('welcome.html', club=club, competitions=competitions,
-                               is_past_competition=is_past_competition)
+                               is_past_competition=is_past_competition_func)
 
     places_required = int(request.form['places'])
     competition_places = int(competition['numberOfPlaces'])
 
     if places_required > competition_places:
         flash('Not enough places-please try again')
-        return render_template('booking.html', club=club, competition=competition)
+        return render_template('booking.html', club=club, competition=competition,
+                               is_past_competition=is_past_competition_func)
 
     if 12 < places_required < 1:
         flash('Cannot book less than 1 or more than 12 places')
-        return render_template('booking.html', club=club, competition=competition)
-
+        return render_template('booking.html', club=club, competition=competition,
+                               is_past_competition=is_past_competition_func)
     if places_required > club_points:
         flash('Not enough points-please try again')
-        return render_template('booking.html', club=club, competition=competition)
-
+        return render_template('booking.html', club=club, competition=competition,
+                               is_past_competition=is_past_competition_func)
     competition['numberOfPlaces'] = competition_places - places_required
     club['points'] = club_points - places_required
     flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions,
+                           is_past_competition=is_past_competition_func)
 
 
 # TODO: Add route for points display
