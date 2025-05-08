6d890d3991d3b7eda14b4e02669f4ce48e8cb6ea
Hedi-Slm <hedi.slm42@gmail.com>
2025-05-08 08:58:53 +0200

    fixed redirection not having past_competition arg

--- server.py
+++ server.py
@@ -53,12 +53,14 @@ def book(competition, club):
     if found_club and found_competition:
         if is_past_competition_func(found_competition['date']):
             flash("Cannot book places for past competitions")
-            return render_template('welcome.html', club=club, competitions=competitions)
+            return render_template('welcome.html', club=club, competitions=competitions,
+                                   is_past_competition=is_past_competition_func)
         else:
             return render_template('booking.html', club=found_club, competition=found_competition)
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions,
+                               is_past_competition=is_past_competition_func)
 
 
 def can_book_places(club, competition, places_requested):
@@ -91,7 +93,7 @@ def purchase_places():
         return render_template('booking.html', club=club, competition=competition)
 
     deduct_places(club, competition, places_required)
-    flash('Great-booking complete!')
+    flash('Great, booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions,
                            is_past_competition=is_past_competition_func)
 
